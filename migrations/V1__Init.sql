0411862f21ab2a226fbfe49dd42b64a6a4f41ab2
YudoTLE <yudojago@gmail.com>
2026-09-19 16:33:31 +0700

    outbox

--- migrations/V1__Init.sql
+++ migrations/V1__Init.sql
@@ -5,3 +5,13 @@ CREATE TABLE images (
     create_time TIMESTAMP(6) NOT NULL,
     update_time TIMESTAMP(6) NOT NULL
 ) ENGINE = InnoDB;
+
+CREATE TABLE outbox_messages (
+    id             CHAR(36) PRIMARY KEY,
+    partition_key  TEXT,
+    aggregate_id   TEXT NOT NULL,
+    aggregate_type TEXT NOT NULL,
+    type           TEXT NOT NULL,
+    payload        JSON NOT NULL,
+    occur_time     TIMESTAMP(6) NOT NULL
+) ENGINE = InnoDB;
